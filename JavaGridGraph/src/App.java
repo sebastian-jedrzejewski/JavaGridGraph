a401860fb0ab90a6cc89552975c7bfa1a44d3d82
Mateusz Skoczek <mateusz5432160@gmail.com>
2022-05-04 12:17:49 +0200

    JavaFX code added

--- JavaGridGraph/src/App.java
+++ JavaGridGraph/src/App.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("JavaGridGraph");
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("JavaGridGraph");
+
+        Label label = new Label("Hello World");
+        label.setPadding(new Insets(10));
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
