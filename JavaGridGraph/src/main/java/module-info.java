64fdd8c6ee4982bc9fb1d67da3c52db161bad78f
Mateusz Skoczek <mateusz5432160@gmail.com>
2022-05-24 11:25:28 +0200

    działa

---
+++ JavaGridGraph/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module JavaGridGraph {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens App;
+}
---
+++ JavaGridGraph/src/test/java/Core/Helpers/PriorityQueueTest.java
@@ -0,0 +1,11 @@
+package Core.Helpers;
+
+import static org.junit.Assert.*;
+
+public class PriorityQueueTest {
+
+    @org.junit.Test
+    public void getPosition() {
+        System.out.println("test");
+    }
+}
